ca13f19f0a21281a29065927fa38bec257647775
Takuya Horikawa <horikawa_takuya@benic.co.jp>
2024-05-22 23:15:15 +0000

    first commit

--- README.md
+++ README.md
@@ -36,3 +36,10 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+
+## UI5 Web Component
+```bash
+npm i @ui5/webcomponents
+npm i @ui5/webcomponents-fiori
+```
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import "@ui5/webcomponents/dist/Avatar";
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents-fiori/dist/ShellBar";
+import "@ui5/webcomponents-icons/dist/customer";
+import "@ui5/webcomponents-icons/dist/nav-back";
+</script>
+<ui5-shellbar primary-title="Corporate Portal" secondary-title="Secondary title">
+    <ui5-avatar slot="profile" icon="customer"></ui5-avatar>
+    <img slot="logo" src="https://sap.github.io/ui5-webcomponents/images/sap-logo-svg.svg" />
+    <ui5-button icon="nav-back" slot="startButton"></ui5-button>
+</ui5-shellbar>
+<slot></slot>
